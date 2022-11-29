f124d5ad2ce45d94f89b9ca084d102324329fd26
Luqianyou <757628817@qq.com>
2022-11-29 12:40:18 +0800

    http test

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-console.log(321);
+const http = require('http')
+
+const server = http.createServer((req, res) =>{
+  const data = "<input />"
+
+  const jsonData = JSON.stringify(data)
+
+  res.writeHead(200,{
+    'Content-Type': 'text/html' 
+  })
+
+  res.write(data)
+
+  res.end();
+})
+
+server.listen(8000, () =>{
+  console.log('服务器已启动');
+})
